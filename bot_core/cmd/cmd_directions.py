f0aab35e1711b778c40a960bdc0f7430eceac9ac
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2022-07-31 22:46:57 +0500

    Update cmd_directions.py

--- bot_core/cmd/cmd_directions.py
+++ bot_core/cmd/cmd_directions.py
@@ -13,7 +13,7 @@ async def send_directions_raw(user_id: int, chat_id: int = None):
   await init.bot.set_state(user_id, UserState.set_edu_direction)
   user = await init.bot.current_states.get_data(chat_id, user_id)
   markup = build_directions_markup(user['directions'])
-  return await init.bot.send_message(chat_id, "Выбери направление обучения:", reply_markup=markup)
+  return await init.bot.send_message(chat_id, "Выбери категорию обучения:", reply_markup=markup)
 
 async def process_directions(call: CallbackQuery):
   user = await init.bot.current_states.get_data(call.message.chat.id, call.from_user.id)
@@ -35,12 +35,12 @@ async def process_directions(call: CallbackQuery):
     return
 
   if len(user["directions"]) == 0:
-    await init.bot.answer_callback_query(call.id, text="Направления не выбранны!", show_alert=True)
+    await init.bot.answer_callback_query(call.id, text="Категории не выбраны!", show_alert=True)
     return
 
-  await init.bot.answer_callback_query(call.id, text="Направления выбранны!")
+  await init.bot.answer_callback_query(call.id, text="Категории выбранны!")
   ob = '\n'.join(map(lambda e: str(f"- *{edu_directions[e]}*"), user["directions"]))
-  await init.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Направление:\n{ob}", parse_mode='Markdown')
+  await init.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Категория:\n{ob}", parse_mode='Markdown')
 
   await cmd_edutype.send_edutypes_raw(call.from_user.id)
 
